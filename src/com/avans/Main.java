1cbb37ec4f46855b2b76785c20e745d1794a8c97
Maurice Snoeren <mac.snoeren@avans.nl>
2021-02-03 11:09:25 +0100

    Added SayHello method

--- src/com/avans/Main.java
+++ src/com/avans/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Students!");
     }
+
+    public static void SayHello() {
+        System.out.println("Hello");
+    }
 }
