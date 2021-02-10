c6f0fee03808d10e5dbe3f3b9f2dad7dbd37de8a
Maurice Snoeren <mac.snoeren@avans.nl>
2021-02-10 13:37:04 +0100

    cleaned up some code

--- src/com/avans/Main.java
+++ src/com/avans/Main.java
@@ -3,20 +3,6 @@ package com.avans;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Hello Students!");
-
-        System.out.println("never gonna give you up");
-
-        martijnWasHere();
-    }
-    
-    public static void martijnWasHere() {
-        System.out.println("Martijn was here!");
-    }
-
-    // Capital S? Bad!
-    public static void SayHello() {
-        System.out.println("Hello");
     }
 }
