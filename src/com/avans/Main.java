a63888691ce67eafe188c54640e6c34acb08c429
Maurice Snoeren <mac.snoeren@avans.nl>
2021-02-10 13:46:28 +0100

    Added method sum and nice debug feature!!

--- src/com/avans/Main.java
+++ src/com/avans/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Students!");
+        System.out.println(sum(12, 12));
+        debug(10, "A fatal error occurred!");
     }
+
+    public static int sum (int a, int b) {
+        return a + b;
+    }
+
+    public static void debug( int debugLevel, String debug ) {
+        System.out.println("DEBUG (" + debugLevel + "): " + debug);
+    }
+
 }
